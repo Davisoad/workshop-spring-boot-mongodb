c8205ed9375a915f8093415d21453f256d8f78ef
Davisoad <davibragaz18@gmail.com>
2022-12-21 10:58:52 -0300

    projeto criado

---
+++ src/test/java/com/javadavysec/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javadavysec.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
