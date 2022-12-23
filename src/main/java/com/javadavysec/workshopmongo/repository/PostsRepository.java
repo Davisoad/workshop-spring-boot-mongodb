a318339d71742c224861997a19b3c399036575ab
Davisoad <davibragaz18@gmail.com>
2022-12-23 10:47:33 -0300

    Consulta simples com @Query

--- src/main/java/com/javadavysec/workshopmongo/repository/PostsRepository.java
+++ src/main/java/com/javadavysec/workshopmongo/repository/PostsRepository.java
@@ -3,6 +3,7 @@ package com.javadavysec.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.javadavysec.workshopmongo.domain.Post;
@@ -10,5 +11,8 @@ import com.javadavysec.workshopmongo.domain.Post;
 @Repository
 public interface PostsRepository extends MongoRepository<Post, String> {
     
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> serachTitle(String text);
+
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
